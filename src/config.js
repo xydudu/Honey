accbdc959581a62236b431cdf615b14d33e28099
Lian Hsueh <xuedudu@gmail.com>
2012-06-13 11:24:20 +0800

    rebuild with TDD

---
+++ src/config.js
@@ -0,0 +1,12 @@
+// project config
+
+var CONFIG = {
+
+    PROJECT : 'i.hunantv', //绑定到项目中
+    VERSION : '20120613',
+    DEV : true, 
+    COMBO : false,
+    COMBOURL : 'http://honey.hunantv.com/combo/',
+    ROOT : 'http://honey.hunantv.com/js/'
+
+};
---
+++ src/honey.source.js
@@ -0,0 +1,15 @@
+/*
+ * 
+ * Honey, just a loader, based headjs ( headjs.com )
+ * Version 2.0
+ * Lian Hsueh from Huantv.com
+ *
+ */
+
+(function(doc, undefined) {
+    
+    
+    
+
+})(document);
+
