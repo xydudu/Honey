0d0ca8a2177560458f442e431978bbd18b59e5ba
Lian Hsueh <xuedudu@gmail.com>
2012-10-12 15:53:37 +0800

    deploy

---
+++ src/config.imgotv.js
@@ -0,0 +1,9 @@
+// project config
+var
+PROJECT = 'imgotv', //绑定到项目中
+VERSION = '20120928',
+DEV = false, 
+COMBO = false,
+COMBOURL = 'http://honey.hunantv.com/combo/',
+ROOT = 'http://img.imgo.tv/ui/honey2.0/';
+//ROOT = 'http://lab.local:3000'
