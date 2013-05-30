0e0c53233a8942ba9038c0ddc1cd34da6dc601b8
Lian Hsueh <xuedudu@gmail.com>
2013-05-30 17:30:25 +0800

    verify code

--- src/widget/comment.source.js
+++ src/widget/comment.source.js
@@ -203,9 +203,8 @@ honey.def('lib:jquery, lib:mustache', function(H) {
             reply_box = $('#reply-'+ id)
             
             if (need_verify) {
+                var src = api +'/comment/checkcode'
                 $('.reply .verify-img').each(function(_no, _img) {
-                    console.log(_img)
-                    var src = _img.src.split('?')[0] 
                     _img.src = src +'?'+ Math.random()
                 })
             }
