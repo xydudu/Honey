4b035f90b0db3aa4be57b780c0e8eeb7147f25fb
Lian Hsueh <xuedudu@gmail.com>
2014-01-07 17:55:16 +0800

    add method: honey.config

--- test/honey.js
+++ test/honey.js
@@ -10,14 +10,33 @@ describe("Honey", function () {
         honey.should.have.property('config');
     });
 
-    describe('配置项:传入项目名', function() {
-        before(function(){
-            honey.config('i.hunantv')
-        }) 
-        it("自动取得相关配置", function() {
-
-        });
+    describe('配置项:直接传入项目配置文件URL', function() {
+        this.timeout(2000)
+        it("自动取得相关配置", function(done) {
+            var config_url = 'http://honey.hunantv.com/src/configs/config.ihunantv.js' 
+            honey.config(config_url, function() {
+                PROJECT.should.equal('i.hunantv')
+                ROOT.should.equal('http://honey.hunantv.com/i/js')
+                done()
+            })
+        })
     })
+
+    describe('配置项:直接传入配置项', function() {
+        it("手动传入单项配置", function() {
+            honey.config('PROJECT', 'data.hunantv')
+            PROJECT.should.equal('data.hunantv')
+        })
+        it("手动传入多项配置", function() {
+            honey.config({
+                'ROOT': 'http://honey.hunantv.com/data/js',
+                'PUBROOT': 'http://honey.hunantv.com/honey-2.0/'
+            })
+            ROOT.should.equal('http://honey.hunantv.com/data/js')
+            PUBROOT.should.equal('http://honey.hunantv.com/honey-2.0/')
+        })
+    })
+
     
 
     //it("引入一个文件", function(done) {
